f5482728ce15b9727784e2e8b7a292bf217f2cfc
Cai Junhao <caijh28@mail2.sysu.edu.cn>
2018-02-04 10:14:56 +0800

    add senet

--- hparams.py
+++ hparams.py
@@ -57,7 +57,7 @@ def create_target_hparams(hparam_string=None):
 
 def create_finetune_hparams(hparam_string=None):
     hparams = tf.contrib.training.HParams(learning_rate=0.001,
-                                          lr_decay_step=10000,
+                                          lr_decay_step=50000,
                                           lr_decay_rate=0.95,
                                           dropout_keep_prob=0.7,
                                           batch_size=64,
@@ -121,6 +121,26 @@ def create_domain_adapt_se_hparams(hparam_string=None):
                                           reduction_ratio=16,
                                           lamb=0.025,
                                           scope='domain_adapt_se')
+
+    if hparam_string:
+        tf.logging.info('Parsing command line hparams: %s', hparam_string)
+        hparams.parse(hparam_string)
+
+    tf.logging.info('Final parsed hparams: %s', hparams.values())
+    return hparams
+
+
+def create_da_full_senet_hparams(hparam_string=None):
+    hparams = tf.contrib.training.HParams(learning_rate=0.01,
+                                          lr_decay_step=10000,
+                                          lr_decay_rate=0.95,
+                                          dropout_keep_prob=0.7,
+                                          batch_size=32,
+                                          image_size=224,
+                                          adapt_dims=128,
+                                          reduction_ratio=16,
+                                          lamb=0.025,
+                                          scope='da_full_senet')
 
     if hparam_string:
         tf.logging.info('Parsing command line hparams: %s', hparam_string)
