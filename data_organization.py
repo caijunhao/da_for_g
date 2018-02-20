2c52417a34a30666cd0d6da646a9af2fb104cd08
Cai Junhao <caijh28@mail2.sysu.edu.cn>
2018-02-20 11:52:57 +0800

    add argument: argumented

--- data_organization.py
+++ data_organization.py
@@ -8,6 +8,7 @@ parser = argparse.ArgumentParser(description='organize data collected by our own
 parser.add_argument('--data_path', required=True, type=str, help='path to the data set.')
 parser.add_argument('--label_path', required=True, type=str, help='path to the label txt file.')
 parser.add_argument('--output_path', required=True, type=str, help='path to the organized data.')
+parser.add_argument('--argumented', default=False, type=str, help='whether to argument the data set.')
 args = parser.parse_args()
 
 
@@ -35,15 +36,28 @@ def main():
         angle_index = int(grasp_index)
         label = int(label)
         img = cv2.imread(os.path.join(args.data_path, file_name))
-        for j in range(4):
-            grasp_angle = (((angle_index + j * 9) % 18) * 10 - 90) * 1.0 / 180 * pi
-            rotated_img = img if j == 0 else cv2.rotate(img, j - 1)
+        if args.argumented:
+            for j in range(4):
+                grasp_angle = (((angle_index + j * 9) % 18) * 10 - 90) * 1.0 / 180 * pi
+                rotated_img = img if j == 0 else cv2.rotate(img, j - 1)
+                if label == 0:
+                    cv2.imwrite(positive_data_path + '/{:06d}.jpg'.format(i * 4 + j + num_data), rotated_img)
+                    f_info_positive.write(
+                        ','.join(['{:06d}.jpg'.format(i * 4 + j + num_data), '{}\n'.format(grasp_angle)]))
+                else:
+                    cv2.imwrite(negative_data_path + '/{:06d}.jpg'.format(i * 4 + j + num_data), rotated_img)
+                    f_info_negative.write(
+                        ','.join(['{:06d}.jpg'.format(i * 4 + j + num_data), '{}\n'.format(grasp_angle)]))
+        else:
+            grasp_angle = (angle_index * 10 - 90) * 1.0 / 180 * pi
             if label == 0:
-                cv2.imwrite(positive_data_path+'/{:06d}.jpg'.format(i*4+j+num_data), rotated_img)
-                f_info_positive.write(','.join(['{:06d}.jpg'.format(i*4+j+num_data), '{}\n'.format(grasp_angle)]))
+                cv2.imwrite(positive_data_path + '/{:06d}.jpg'.format(i + num_data), img)
+                f_info_positive.write(
+                    ','.join(['{:06d}.jpg'.format(i + num_data), '{}\n'.format(grasp_angle)]))
             else:
-                cv2.imwrite(negative_data_path+'/{:06d}.jpg'.format(i*4+j+num_data), rotated_img)
-                f_info_negative.write(','.join(['{:06d}.jpg'.format(i*4+j+num_data), '{}\n'.format(grasp_angle)]))
+                cv2.imwrite(negative_data_path + '/{:06d}.jpg'.format(i + num_data), img)
+                f_info_negative.write(
+                    ','.join(['{:06d}.jpg'.format(i + num_data), '{}\n'.format(grasp_angle)]))
     f_info_positive.close()
     f_info_negative.close()
 
