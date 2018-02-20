3955fe0bf83051c848fa24e7b5f0a8d655d32b51
Cai Junhao <caijh28@mail2.sysu.edu.cn>
2018-02-20 15:06:26 +0800

    add model hyper parameters

--- hparams.py
+++ hparams.py
@@ -96,7 +96,7 @@ def create_domain_adapt_hparams(hparam_string=None):
                                           lr_decay_step=10000,
                                           lr_decay_rate=0.95,
                                           dropout_keep_prob=0.7,
-                                          batch_size=32,
+                                          batch_size=64,
                                           image_size=224,
                                           adapt_dims=128,
                                           lamb=0.025,
@@ -115,7 +115,7 @@ def create_domain_adapt_se_hparams(hparam_string=None):
                                           lr_decay_step=10000,
                                           lr_decay_rate=0.95,
                                           dropout_keep_prob=0.7,
-                                          batch_size=32,
+                                          batch_size=64,
                                           image_size=224,
                                           adapt_dims=128,
                                           reduction_ratio=16,
@@ -130,17 +130,98 @@ def create_domain_adapt_se_hparams(hparam_string=None):
     return hparams
 
 
+def create_domain_adapt_se2_hparams(hparam_string=None):
+    hparams = tf.contrib.training.HParams(learning_rate=0.01,
+                                          lr_decay_step=10000,
+                                          lr_decay_rate=0.95,
+                                          dropout_keep_prob=0.7,
+                                          batch_size=64,
+                                          image_size=224,
+                                          adapt_dims=128,
+                                          reduction_ratio=16,
+                                          lamb=0.025,
+                                          scope='domain_adapt_se2')
+
+    if hparam_string:
+        tf.logging.info('Parsing command line hparams: %s', hparam_string)
+        hparams.parse(hparam_string)
+
+    tf.logging.info('Final parsed hparams: %s', hparams.values())
+    return hparams
+
+
 def create_da_full_senet_hparams(hparam_string=None):
     hparams = tf.contrib.training.HParams(learning_rate=0.01,
                                           lr_decay_step=10000,
                                           lr_decay_rate=0.95,
                                           dropout_keep_prob=0.7,
-                                          batch_size=32,
+                                          batch_size=64,
                                           image_size=224,
                                           adapt_dims=128,
                                           reduction_ratio=16,
                                           lamb=0.025,
                                           scope='da_full_senet')
+
+    if hparam_string:
+        tf.logging.info('Parsing command line hparams: %s', hparam_string)
+        hparams.parse(hparam_string)
+
+    tf.logging.info('Final parsed hparams: %s', hparams.values())
+    return hparams
+
+
+def create_da_full_senet2_hparams(hparam_string=None):
+    hparams = tf.contrib.training.HParams(learning_rate=0.01,
+                                          lr_decay_step=10000,
+                                          lr_decay_rate=0.95,
+                                          dropout_keep_prob=0.7,
+                                          batch_size=64,
+                                          image_size=224,
+                                          adapt_dims=128,
+                                          reduction_ratio=16,
+                                          lamb=0.025,
+                                          scope='da_full_senet2')
+
+    if hparam_string:
+        tf.logging.info('Parsing command line hparams: %s', hparam_string)
+        hparams.parse(hparam_string)
+
+    tf.logging.info('Final parsed hparams: %s', hparams.values())
+    return hparams
+
+
+def create_domain_adapt_multi_mmd_se_hparams(hparam_string=None):
+    hparams = tf.contrib.training.HParams(learning_rate=0.02,
+                                          lr_decay_step=100000,
+                                          lr_decay_rate=0.90,
+                                          dropout_keep_prob=0.7,
+                                          batch_size=64,
+                                          image_size=224,
+                                          adapt_dims=128,
+                                          reduction_ratio=16,
+                                          lamb1=0.0125,
+                                          lamb2=0.0125,
+                                          scope='multi_mmd_se')
+
+    if hparam_string:
+        tf.logging.info('Parsing command line hparams: %s', hparam_string)
+        hparams.parse(hparam_string)
+
+    tf.logging.info('Final parsed hparams: %s', hparams.values())
+    return hparams
+
+
+def create_semi_supervised_domain_adapt_hparams(hparam_string=None):
+    hparams = tf.contrib.training.HParams(learning_rate=0.001,
+                                          lr_decay_step=27000,
+                                          lr_decay_rate=0.90,
+                                          dropout_keep_prob=0.7,
+                                          batch_size=64,
+                                          image_size=224,
+                                          adapt_dims=128,
+                                          lamb1=0.0125,
+                                          lamb2=0.0125,
+                                          scope='semi_supervised_domain_adapt')
 
     if hparam_string:
         tf.logging.info('Parsing command line hparams: %s', hparam_string)
